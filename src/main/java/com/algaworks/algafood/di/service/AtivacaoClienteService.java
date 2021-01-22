25972f68895129bb79761a2f300f593f2826130b
israelnp <israelpereira1984@gmail.com>
2021-01-22 13:04:55 -0300

    02.22-publicando-e-consumindo-eventos-customizados

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -4,37 +4,21 @@ import com.algaworks.algafood.di.notificacao.NivelDeUrgencia;
 import com.algaworks.algafood.di.notificacao.Notificador;
 import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Component
 public class AtivacaoClienteService {
 
-	@TipoDoNotificador(NivelDeUrgencia.SEM_URGENCIA)
 	@Autowired
-	private Notificador notificador;
+	private ApplicationEventPublisher eventPublisher;
 
-//	public AtivacaoClienteService(Notificador notificador) {
-//		this.notificador = notificador;
-//
-//		System.out.println("AtivacaoClienteService: " + notificador);
-//	}
-	@PostConstruct
-	public void init(){
-		System.out.println("INIT " + notificador);
-	}
-	public void ativar(Cliente cliente) {
+	public void ativar(Cliente cliente){
 		cliente.ativar();
-		notificador.notificar(cliente, "seu cadastro no sistema está ativo!");
-	}
-
-	@PreDestroy
-	public void destroy(){
-		System.out.println("DESTROY");
+		eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
 	}
 
 	
