dae18e3989f14cb4f4f363fd8ab69d3e3939967d
israelnp <israelpereira1984@gmail.com>
2021-01-22 13:26:58 -0300

    02.25-criando-e-acessando-propriedades-customizadas-com-value

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.di.notificacao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -11,14 +12,17 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 
-	@Autowired
-	private NotificadorProperties properties;
+	@Value("${notificador.email.host-servidor}")
+	private String host;
+
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 
-		System.out.println("Host: " + properties.getHostServidor());
-		System.out.println("Porta: " + properties.getPortaServidor());
+		System.out.println("Host: " + host);
+		System.out.println("Porta: " + porta);
 
 		System.out.printf("Notificando %s através do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
