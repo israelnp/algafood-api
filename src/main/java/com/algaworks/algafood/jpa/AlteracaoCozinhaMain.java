18845e6a4cc9ae6f3188a62c9dd56d41cc13e4e2
israelnp <israelpereira1984@gmail.com>
2021-01-25 09:43:10 -0300

    03.11-atualizando-um-objeto-no-banco-de-dados

--- src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
@@ -1,4 +1,6 @@
 package com.algaworks.algafood.jpa;
+
+
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -6,7 +8,7 @@ import org.springframework.context.ApplicationContext;
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
@@ -15,17 +17,12 @@ public class InclusaoCozinhaMain {
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Brasileira");
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasileira");
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Japonesa");
+        cadastroCozinha.salvar(cozinha);
 
-        cozinha1 = cadastroCozinha.salvar(cozinha1);
-        cozinha2 = cadastroCozinha.salvar(cozinha2);
-
-        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
     }
 
 }
--- src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
@@ -3,7 +3,7 @@ package com.algaworks.algafood.jpa;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;q
+import org.springframework.context.ApplicationContext;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
