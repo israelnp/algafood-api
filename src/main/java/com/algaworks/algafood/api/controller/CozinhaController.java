8cf25549db3101ed5b6445c3fedeee66eb6d8c8f
israelnp <israelpereira1984@gmail.com>
2021-01-26 09:58:57 -0300

    04.14-consultando-singleton-resource-com-get-e-pathvariable

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +30,9 @@ public class CozinhaController {
         System.out.println("LISTAR 2");
         return cozinhaRepository.listar();
     }
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId){
+        return cozinhaRepository.buscar(cozinhaId);
+    }
+
 }
