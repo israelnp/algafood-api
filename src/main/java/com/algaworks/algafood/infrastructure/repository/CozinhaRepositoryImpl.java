459cbb7e22f76b70a6bc78552c5ab4f6cb5c90ba
israelnp <israelpereira1984@gmail.com>
2021-01-28 08:56:17 -0300

    05.01-implementando-consultas-jpql-em-repositorios

--- src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -20,6 +20,14 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
     }
 
     @Override
+    public List<Cozinha> consultarPorNome(String nome) {
+        return manager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
+                .setParameter("nome", nome)
+                .getResultList();
+
+    }
+
+    @Override
     public Cozinha buscar(Long id) {
 
          Cozinha cozinha =manager.find(Cozinha.class, id);
