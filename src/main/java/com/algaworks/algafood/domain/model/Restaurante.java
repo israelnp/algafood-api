f655bdc32862687bfb1c2eef34e643d035ae6c06
israelnp <israelpereira1984@gmail.com>
2021-02-09 11:17:37 -0300

    09.16-criando-constraints-de-validacao-customizadas-com-implementacao-de-constraintvalidator

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
 import com.algaworks.algafood.core.Groups;
+import com.algaworks.algafood.core.Multiplo;
 import com.algaworks.algafood.core.TaxaFrete;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
@@ -36,6 +37,7 @@ public class Restaurante {
     //@DecimalMin("0")
     @NotNull
     @TaxaFrete
+    @Multiplo(numero = 5)
    // @PositiveOrZero(message = "{TaxaFrete.invalida}")
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
