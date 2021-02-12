003ab788bfc79dc2d69eb622eb629818af276042
israelnp <israelpereira1984@gmail.com>
2021-02-12 10:30:11 -0300

    11.04-desafio-usando-jsonignoreproperties-e-jackson-mixin

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -1,5 +1,9 @@
 package com.algaworks.algafood.core.jackson;
 
+import com.algaworks.algafood.api.model.mixin.CidadeMixin;
+import com.algaworks.algafood.api.model.mixin.CozinhaMixin;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
@@ -13,6 +17,8 @@ public class JacksonMixinModule extends SimpleModule {
 
     public JacksonMixinModule() {
         setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+        setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
+        setMixInAnnotation(Cidade.class, CidadeMixin.class);
     }
 
 }
