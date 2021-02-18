403dd28a84426d88561a184c16619d5e9f2f2823
israelnp <israelpereira1984@gmail.com>
2021-02-18 15:49:41 -0300

    12.20-otimizando-a-query-de-pedidos-e-retornando-model-resumido-na-listagem

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import com.algaworks.algafood.api.assembler.PedidoResumoModelAssembler;
+import com.algaworks.algafood.api.model.PedidoResumoModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,11 +29,14 @@ public class PedidoController {
     @Autowired
     private PedidoModelAssembler pedidoModelAssembler;
 
+    @Autowired
+    private PedidoResumoModelAssembler pedidoResumoModelAssembler;
+
     @GetMapping
-    public List<PedidoModel> listar() {
+    public List<PedidoResumoModel> listar() {
         List<Pedido> todosPedidos = pedidoRepository.findAll();
 
-        return pedidoModelAssembler.toCollectionModel(todosPedidos);
+        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
     }
 
     @GetMapping("/{pedidoId}")
--- src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
@@ -1,10 +1,16 @@
 package com.algaworks.algafood.domain.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Pedido;
 
+import java.util.List;
+
 @Repository
 public interface PedidoRepository extends CustomJpaRepository<Pedido, Long> {
+
+    @Query("from Pedido p join fetch p.cliente join fetch p.restaurante r join fetch r.cozinha")
+    List<Pedido> findAll();
 
 }
