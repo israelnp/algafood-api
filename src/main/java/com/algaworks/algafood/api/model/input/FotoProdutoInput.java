15f038261a6aa5cafe2b8ebed898821ce50ad877
israelnp <israelpereira1984@gmail.com>
2021-03-02 11:03:16 -0300

    14.03-validando-o-tamanho-maximo-do-arquivo

--- src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
@@ -1,14 +1,18 @@
 package com.algaworks.algafood.api.model.input;
 
+import com.algaworks.algafood.core.validation.FileSize;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class FotoProdutoInput {
-
+    @NotNull
+    @FileSize(max = "500KB")
     private MultipartFile arquivo;
     private String descricao;
 
