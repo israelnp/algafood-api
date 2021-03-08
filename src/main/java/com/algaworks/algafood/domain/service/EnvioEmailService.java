4f574de1148c588d806f77ceec91242f758e996f
israelnp <israelpereira1984@gmail.com>
2021-03-08 12:12:59 -0300

    15.05-processando-template-do-corpo-de-emails-com-apache-freemarker

--- src/main/java/com/algaworks/algafood/domain/service/EnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/domain/service/EnvioEmailService.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.domain.service;
 
+import java.util.Map;
 import java.util.Set;
 
 import lombok.Builder;
@@ -18,6 +19,8 @@ public interface EnvioEmailService {
         private Set<String> destinatarios;
         private String assunto;
         private String corpo;
+        @Singular("variavel")
+        private Map<String, Object> variaveis;
 
     }
 
