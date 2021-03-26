7e1ad2a7d840e82ed9d705a91554bd5d13425c4a
israelnp <israelpereira1984@gmail.com>
2021-03-26 15:26:50 -0300

    19.39-corrigindo-as-propriedades-de-links-na-documentacao

--- src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
@@ -8,12 +8,14 @@ import java.net.URLStreamHandler;
 import java.util.Arrays;
 import java.util.List;
 
+import com.algaworks.algafood.api.openapi.model.LinksModelOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Links;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -67,6 +69,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 						URL.class, URI.class, URLStreamHandler.class, Resource.class,
 						File.class, InputStream.class)
 				.directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
+				.directModelSubstitute(Links.class, LinksModelOpenApi.class)
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(Page.class, CozinhaModel.class),
 						CozinhasModelOpenApi.class))
