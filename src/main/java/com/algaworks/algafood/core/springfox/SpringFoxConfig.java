289ba1019c9833c8f2b0c552ea070435ccf2b114
israelnp <israelpereira1984@gmail.com>
2021-03-26 16:59:35 -0300

    19.45-desafio-corrigindo-a-documentacao-dos-endpoints-de-pedidos-paginacao

--- src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
@@ -74,8 +74,9 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 						CozinhasModelOpenApi.class))
 
 				.alternateTypeRules(AlternateTypeRules.newRule(
-						typeResolver.resolve(Page.class, PedidoResumoModel.class),
+						typeResolver.resolve(PagedModel.class, PedidoResumoModel.class),
 						PedidosResumoModelOpenApi.class))
+
 
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(CollectionModel.class, CidadeModel.class),
