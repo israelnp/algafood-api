504d57e34223218eb59cddcca5b4f5d38f2c4d24
israelnp <israelpereira1984@gmail.com>
2021-03-29 13:04:28 -0300

    20.15-desafio-adicionando-os-recursos-de-cozinhas-na-v2-da-api

--- src/main/java/com/algaworks/algafood/api/v2/AlgaLinksV2.java
+++ src/main/java/com/algaworks/algafood/api/v2/AlgaLinksV2.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.v2;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
+import com.algaworks.algafood.api.v2.controller.CozinhaControllerV2;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,15 @@ public class AlgaLinksV2 {
 
     public Link linkToCidades() {
         return linkToCidades(IanaLinkRelations.SELF.value());
+    }
+
+
+    public Link linkToCozinhas(String rel) {
+        return linkTo(CozinhaControllerV2.class).withRel(rel);
+    }
+
+    public Link linkToCozinhas() {
+        return linkToCozinhas(IanaLinkRelations.SELF.value());
     }
 
 }
