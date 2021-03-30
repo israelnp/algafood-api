53d982a2c43e4e6402a0296ed7411554be1357c1
israelnp <israelpereira1984@gmail.com>
2021-03-30 11:01:46 -0300

    21.04-configurando-o-appender-do-loggly-no-logback

--- src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
@@ -45,10 +45,6 @@ import com.algaworks.algafood.api.v1.openapi.model.PermissoesModelOpenApi;
 import com.algaworks.algafood.api.v1.openapi.model.ProdutosModelOpenApi;
 import com.algaworks.algafood.api.v1.openapi.model.RestaurantesBasicoModelOpenApi;
 import com.algaworks.algafood.api.v1.openapi.model.UsuariosModelOpenApi;
-import com.algaworks.algafood.api.v2.model.CidadeModelV2;
-import com.algaworks.algafood.api.v2.model.CozinhaModelV2;
-import com.algaworks.algafood.api.v2.openapi.model.CidadesModelV2OpenApi;
-import com.algaworks.algafood.api.v2.openapi.model.CozinhasModelV2OpenApi;
 import com.fasterxml.classmate.TypeResolver;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
@@ -71,7 +67,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig implements WebMvcConfigurer {
 
-	//	@Bean
+	@Bean
 	public Docket apiDocketV1() {
 		var typeResolver = new TypeResolver();
 
@@ -147,41 +143,6 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 						new Tag("Permissões", "Gerencia as permissões"));
 	}
 
-	@Bean
-	public Docket apiDocketV2() {
-		var typeResolver = new TypeResolver();
-
-		return new Docket(DocumentationType.SWAGGER_2)
-				.groupName("V2")
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
-				.paths(PathSelectors.ant("/v2/**"))
-				.build()
-				.useDefaultResponseMessages(false)
-				.globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
-				.globalResponseMessage(RequestMethod.POST, globalPostPutResponseMessages())
-				.globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
-				.globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
-				.additionalModels(typeResolver.resolve(Problem.class))
-				.ignoredParameterTypes(ServletWebRequest.class,
-						URL.class, URI.class, URLStreamHandler.class, Resource.class,
-						File.class, InputStream.class)
-				.directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
-				.directModelSubstitute(Links.class, LinksModelOpenApi.class)
-
-				.alternateTypeRules(AlternateTypeRules.newRule(
-						typeResolver.resolve(PagedModel.class, CozinhaModelV2.class),
-						CozinhasModelV2OpenApi.class))
-
-				.alternateTypeRules(AlternateTypeRules.newRule(
-						typeResolver.resolve(CollectionModel.class, CidadeModelV2.class),
-						CidadesModelV2OpenApi.class))
-
-				.apiInfo(apiInfoV2())
-
-				.tags(new Tag("Cidades", "Gerencia as cidades"),
-						new Tag("Cozinhas", "Gerencia as cozinhas"));
-	}
 
 	private List<ResponseMessage> globalGetResponseMessages() {
 		return Arrays.asList(
@@ -238,20 +199,9 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 
 	private ApiInfo apiInfoV1() {
 		return new ApiInfoBuilder()
-				.title("AlgaFood API (Depreciada)")
-				.description("API aberta para clientes e restaurantes.<br>"
-						+ "<strong>Essa versão da API está depreciada e deixará de existir a partir de 01/01/2021. "
-						+ "Use a versão mais atual da API.")
-				.version("1")
-				.contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
-				.build();
-	}
-
-	private ApiInfo apiInfoV2() {
-		return new ApiInfoBuilder()
 				.title("AlgaFood API")
 				.description("API aberta para clientes e restaurantes")
-				.version("2")
+				.version("1")
 				.contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
 				.build();
 	}
