e45045643a22fe8ce5b44c079f5aed84d6d82682
israelnp <israelpereira1984@gmail.com>
2021-04-10 11:28:48 -0300

    23.42-ajustando-a-documentacao-da-api-para-suporte-a-oauth2

--- src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.core.springfox;
 
+
 import java.io.File;
 import java.io.InputStream;
 import java.net.URI;
@@ -49,16 +50,23 @@ import com.fasterxml.classmate.TypeResolver;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.ResourceOwnerPasswordCredentialsGrant;
 import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -70,13 +78,13 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 	@Bean
 	public Docket apiDocketV1() {
 		var typeResolver = new TypeResolver();
-		
+
 		return new Docket(DocumentationType.SWAGGER_2)
 				.groupName("V1")
 				.select()
-					.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
-					.paths(PathSelectors.ant("/v1/**"))
-					.build()
+				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
+				.paths(PathSelectors.ant("/v1/**"))
+				.build()
 				.useDefaultResponseMessages(false)
 				.globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
 				.globalResponseMessage(RequestMethod.POST, globalPostPutResponseMessages())
@@ -88,47 +96,50 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 						File.class, InputStream.class)
 				.directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
 				.directModelSubstitute(Links.class, LinksModelOpenApi.class)
-				
+
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(PagedModel.class, CozinhaModel.class),
 						CozinhasModelOpenApi.class))
-				
+
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(PagedModel.class, PedidoResumoModel.class),
 						PedidosResumoModelOpenApi.class))
-				
+
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(CollectionModel.class, CidadeModel.class),
 						CidadesModelOpenApi.class))
-				
+
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(CollectionModel.class, EstadoModel.class),
 						EstadosModelOpenApi.class))
-				
+
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(CollectionModel.class, FormaPagamentoModel.class),
 						FormasPagamentoModelOpenApi.class))
-				
+
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(CollectionModel.class, GrupoModel.class),
 						GruposModelOpenApi.class))
-				
+
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(CollectionModel.class, PermissaoModel.class),
 						PermissoesModelOpenApi.class))
-				
+
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(CollectionModel.class, ProdutoModel.class),
 						ProdutosModelOpenApi.class))
-				
+
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(CollectionModel.class, RestauranteBasicoModel.class),
 						RestaurantesBasicoModelOpenApi.class))
-				
+
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(CollectionModel.class, UsuarioModel.class),
 						UsuariosModelOpenApi.class))
-				
+
+				.securitySchemes(Arrays.asList(securityScheme()))
+				.securityContexts(Arrays.asList(securityContext()))
+
 				.apiInfo(apiInfoV1())
 				.tags(new Tag("Cidades", "Gerencia as cidades"),
 						new Tag("Grupos", "Gerencia os grupos de usuários"),
@@ -142,60 +153,89 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 						new Tag("Estatísticas", "Estatísticas da AlgaFood"),
 						new Tag("Permissões", "Gerencia as permissões"));
 	}
-	
+
+	private SecurityScheme securityScheme() {
+		return new OAuthBuilder()
+				.name("AlgaFood")
+				.grantTypes(grantTypes())
+				.scopes(scopes())
+				.build();
+	}
+
+	private SecurityContext securityContext() {
+		var securityReference = SecurityReference.builder()
+				.reference("AlgaFood")
+				.scopes(scopes().toArray(new AuthorizationScope[0]))
+				.build();
+
+		return SecurityContext.builder()
+				.securityReferences(Arrays.asList(securityReference))
+				.forPaths(PathSelectors.any())
+				.build();
+	}
+
+	private List<GrantType> grantTypes() {
+		return Arrays.asList(new ResourceOwnerPasswordCredentialsGrant("/oauth/token"));
+	}
+
+	private List<AuthorizationScope> scopes() {
+		return Arrays.asList(new AuthorizationScope("READ", "Acesso de leitura"),
+				new AuthorizationScope("WRITE", "Acesso de escrita"));
+	}
+
 	private List<ResponseMessage> globalGetResponseMessages() {
 		return Arrays.asList(
 				new ResponseMessageBuilder()
-					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-					.message("Erro interno do servidor")
-					.responseModel(new ModelRef("Problema"))
-					.build(),
+						.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+						.message("Erro interno do servidor")
+						.responseModel(new ModelRef("Problema"))
+						.build(),
 				new ResponseMessageBuilder()
-					.code(HttpStatus.NOT_ACCEPTABLE.value())
-					.message("Recurso não possui representação que poderia ser aceita pelo consumidor")
-					.build()
-			);
+						.code(HttpStatus.NOT_ACCEPTABLE.value())
+						.message("Recurso não possui representação que poderia ser aceita pelo consumidor")
+						.build()
+		);
 	}
-	
+
 	private List<ResponseMessage> globalPostPutResponseMessages() {
 		return Arrays.asList(
 				new ResponseMessageBuilder()
-					.code(HttpStatus.BAD_REQUEST.value())
-					.message("Requisição inválida (erro do cliente)")
-					.responseModel(new ModelRef("Problema"))
-					.build(),
+						.code(HttpStatus.BAD_REQUEST.value())
+						.message("Requisição inválida (erro do cliente)")
+						.responseModel(new ModelRef("Problema"))
+						.build(),
 				new ResponseMessageBuilder()
-					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-					.message("Erro interno no servidor")
-					.responseModel(new ModelRef("Problema"))
-					.build(),
+						.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+						.message("Erro interno no servidor")
+						.responseModel(new ModelRef("Problema"))
+						.build(),
 				new ResponseMessageBuilder()
-					.code(HttpStatus.NOT_ACCEPTABLE.value())
-					.message("Recurso não possui representação que poderia ser aceita pelo consumidor")
-					.build(),
+						.code(HttpStatus.NOT_ACCEPTABLE.value())
+						.message("Recurso não possui representação que poderia ser aceita pelo consumidor")
+						.build(),
 				new ResponseMessageBuilder()
-					.code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
-					.message("Requisição recusada porque o corpo está em um formato não suportado")
-					.responseModel(new ModelRef("Problema"))
-					.build()
-			);
+						.code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
+						.message("Requisição recusada porque o corpo está em um formato não suportado")
+						.responseModel(new ModelRef("Problema"))
+						.build()
+		);
 	}
-	
+
 	private List<ResponseMessage> globalDeleteResponseMessages() {
 		return Arrays.asList(
 				new ResponseMessageBuilder()
-					.code(HttpStatus.BAD_REQUEST.value())
-					.message("Requisição inválida (erro do cliente)")
-					.responseModel(new ModelRef("Problema"))
-					.build(),
+						.code(HttpStatus.BAD_REQUEST.value())
+						.message("Requisição inválida (erro do cliente)")
+						.responseModel(new ModelRef("Problema"))
+						.build(),
 				new ResponseMessageBuilder()
-					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-					.message("Erro interno no servidor")
-					.responseModel(new ModelRef("Problema"))
-					.build()
-			);
+						.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+						.message("Erro interno no servidor")
+						.responseModel(new ModelRef("Problema"))
+						.build()
+		);
 	}
-	
+
 	private ApiInfo apiInfoV1() {
 		return new ApiInfoBuilder()
 				.title("AlgaFood API")
@@ -204,14 +244,14 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
 				.build();
 	}
-	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html")
-			.addResourceLocations("classpath:/META-INF/resources/");
-		
+				.addResourceLocations("classpath:/META-INF/resources/");
+
 		registry.addResourceHandler("/webjars/**")
-			.addResourceLocations("classpath:/META-INF/resources/webjars/");
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
-	
+
 }
