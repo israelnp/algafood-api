caa622a57625ba7a1ebf9527ca96edd8ecf3cce9
israelnp <israelpereira1984@gmail.com>
2021-04-10 11:49:29 -0300

    23.44-customizando-a-pagina-de-oauth2-approval

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -29,77 +29,77 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private JwtKeyStoreProperties jwtKeyStoreProperties;
-	
+
 	@Autowired
 	private DataSource dataSource;
-	
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.jdbc(dataSource);
 	}
-	
+
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 //		security.checkTokenAccess("isAuthenticated()");
 		security.checkTokenAccess("permitAll()")
-			.tokenKeyAccess("permitAll()")
-			.allowFormAuthenticationForClients();
+				.tokenKeyAccess("permitAll()")
+				.allowFormAuthenticationForClients();
 	}
-	
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		var enhancerChain = new TokenEnhancerChain();
 		enhancerChain.setTokenEnhancers(
 				Arrays.asList(new JwtCustomClaimsTokenEnhancer(), jwtAccessTokenConverter()));
-		
+
 		endpoints
-			.authenticationManager(authenticationManager)
-			.userDetailsService(userDetailsService)
-			.reuseRefreshTokens(false)
-			.accessTokenConverter(jwtAccessTokenConverter())
-			.tokenEnhancer(enhancerChain)
-			.approvalStore(approvalStore(endpoints.getTokenStore()))
-			.tokenGranter(tokenGranter(endpoints));
+				.authenticationManager(authenticationManager)
+				.userDetailsService(userDetailsService)
+				.reuseRefreshTokens(false)
+				.accessTokenConverter(jwtAccessTokenConverter())
+				.tokenEnhancer(enhancerChain)
+				.approvalStore(approvalStore(endpoints.getTokenStore()))
+				.tokenGranter(tokenGranter(endpoints));
 	}
-	
+
 	private ApprovalStore approvalStore(TokenStore tokenStore) {
 		var approvalStore = new TokenApprovalStore();
 		approvalStore.setTokenStore(tokenStore);
-		
+
 		return approvalStore;
 	}
-	
+
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		var jwtAccessTokenConverter = new JwtAccessTokenConverter();
-		
+
 		var keyStorePass = jwtKeyStoreProperties.getPassword();
 		var keyPairAlias = jwtKeyStoreProperties.getKeypairAlias();
-		
+
 		var keyStoreKeyFactory = new KeyStoreKeyFactory(
 				jwtKeyStoreProperties.getJksLocation(), keyStorePass.toCharArray());
 		var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
-		
+
 		jwtAccessTokenConverter.setKeyPair(keyPair);
-		
+
 		return jwtAccessTokenConverter;
 	}
-	
+
 	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
 		var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
 				endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
 				endpoints.getOAuth2RequestFactory());
-		
+
 		var granters = Arrays.asList(
 				pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
-		
+
 		return new CompositeTokenGranter(granters);
 	}
-	
+
 }
