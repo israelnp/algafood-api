addf94a805b63df2e84d5195238c060573a6bb3b
israelnp <israelpereira1984@gmail.com>
2021-04-10 09:49:59 -0300

    23.36-configurando-os-clientes-oauth2-em-um-banco-de-dados-sql

--- src/main/java/com/algaworks/algafood/core/security/AlgaSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/AlgaSecurity.java
@@ -1,12 +1,22 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algafood.domain.repository.PedidoRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
 @Component
 public class AlgaSecurity {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
 
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
@@ -16,6 +26,18 @@ public class AlgaSecurity {
         Jwt jwt = (Jwt) getAuthentication().getPrincipal();
 
         return jwt.getClaim("usuario_id");
+    }
+
+    public boolean gerenciaRestaurante(Long restauranteId) {
+        if (restauranteId == null) {
+            return false;
+        }
+
+        return restauranteRepository.existsResponsavel(restauranteId, getUsuarioId());
+    }
+
+    public boolean gerenciaRestauranteDoPedido(String codigoPedido) {
+        return pedidoRepository.isPedidoGerenciadoPor(codigoPedido, getUsuarioId());
     }
 
 }
--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,6 +1,5 @@
 package com.algaworks.algafood.core.security;
 
-
 import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
